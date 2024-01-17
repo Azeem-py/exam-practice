76f4fd4a665e4c8970cc28e60eba6ba29954da7f
Azeem-py <74514285+Azeem-py@users.noreply.github.com>
2024-01-17 13:12:44 +0100

    changed url back to prod. API

--- src/data/url.js
+++ src/data/url.js
@@ -1,5 +1,5 @@
-// export const baseURL = 'https://exam-practice-api-inky.vercel.app'
-// export const hostURL = 'https://exam-practice-chi.vercel.app'
+export const baseURL = 'https://exam-practice-api-inky.vercel.app'
+export const hostURL = 'https://exam-practice-chi.vercel.app'
 
-export const baseURL = 'http://127.0.0.1:3000'
-export const hostURL = 'http://127.0.0.1:5173'
+// export const baseURL = 'http://127.0.0.1:3000'
+// export const hostURL = 'http://127.0.0.1:5173'
